5fc64b5f0df7c66749d214da5e21ae5a301f614d
Михаил Калмыков <68296867+Mike-Vazovskiy@users.noreply.github.com>
2022-04-11 12:02:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//
+new Rellax('.rellax');
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene);
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
